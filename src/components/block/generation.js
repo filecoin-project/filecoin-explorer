0b8c795c61922c51ebff5d5a374f9e797df780bd
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-12-15 17:46:08 -0500

    ui improvements

--- src/components/block/generation.js
+++ src/components/block/generation.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Block from './block';
 import EmptyTipset from './emptyTipset'
-import Separator from './separator'
+import HeightLabel from './heightLabel'
 
 class Generation extends React.Component {
   render() {
     const { blocks, height } = this.props;
     return (
-      <div className="nowrap" style={{ margin: "2rem 0px"}}>
-        <Separator height={height} />
+      <div className="nowrap" style={{ alignItems: 'center', flexDirection: 'row', display: 'flex', margin: "2rem 0px"}}>
+        <HeightLabel height={height} />
         {blocks && blocks.length ? blocks.map((b, i) => {
           return (
             <div className="dib pr4" style={{ width: 250 }} key={i}>
---
+++ src/components/block/heightLabel.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const HeightLabel = ({ height }) => (<p style={{ marginRight: '4em', color: 'grey' }}>{height}</p>
+)
+
+export default HeightLabel
